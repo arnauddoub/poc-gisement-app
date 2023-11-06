91a909c3a428cb06d3f17b4aad8c38a3b7abaa09
ArnaudDoubremelle <arnaud.doub@gmail.com>
2023-11-06 22:31:17 +0100

    deps: install primevue

--- packages/frontend/src/main.ts
+++ packages/frontend/src/main.ts
@@ -1,14 +1,23 @@
-import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import 'primeicons/primeicons.css'
+import '@/styles/main.css'
+
 import App from './App.vue'
 import router from './router'
+
+import PrimeVue from 'primevue/config';
+import Button from 'primevue/button';
+import Tailwind from 'primevue/passthrough/tailwind';
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, { ripple: true, unstyled: true, pt: Tailwind });
+
+app.component('Button', Button);
 
 app.mount('#app')
--- packages/frontend/src/views/HomeView.vue
+++ packages/frontend/src/views/HomeView.vue
@@ -1,9 +1,11 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="flex flex-wrap gap-3 card justify-center">
+    <Button label="Primary" />
+    <Button label="Secondary" severity="secondary" />
+    <Button label="Success" severity="success" />
+    <Button label="Info" severity="info" />
+    <Button label="Warning" severity="warning" />
+    <Button label="Help" severity="help" />
+    <Button label="Danger" severity="danger" />
+  </div>
 </template>
