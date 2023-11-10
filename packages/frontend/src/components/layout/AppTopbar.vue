7a6aff09a1bb32fc38a8c78a3e790655c4b5be96
ArnaudDoubremelle <arnaud.doub@gmail.com>
2023-11-10 23:13:22 +0100

    lint: format

--- packages/frontend/src/components/layout/AppTopbar.vue
+++ packages/frontend/src/components/layout/AppTopbar.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
-import { useLayout } from '@/composables/useLayout'
 import { useToggle } from '@vueuse/core'
 import { isDark } from '@/composables/useDark'
-import { useLayoutStore } from '@/stores/layout';
+import { useLayoutStore } from '@/stores/layout'
 
 const { onMenuToggle } = useLayoutStore()
 const toggleDark = useToggle(isDark)
@@ -11,35 +10,18 @@ const toggleDark = useToggle(isDark)
 <template>
   <div class="layout-topbar">
     <div class="topbar-start">
-      <Button
-        severity="secondary"
-        text
-        class="topbar-menubutton"
-        rounded
-        style="padding: 1px"
-        @click="onMenuToggle()"
-      >
+      <Button severity="secondary" text class="topbar-menubutton" rounded style="padding: 1px" @click="onMenuToggle()">
         <i class="pi pi-bars"></i>
       </Button>
     </div>
     <div class="topbar-end">
       <ul class="flex space-x-4 items-center">
         <li>
-          <Button
-            severity="secondary"
-            :icon="`pi ${isDark ? 'pi-sun' : 'pi-moon'}`"
-            text
-            rounded
-            @click="toggleDark()"
-          />
+          <Button severity="secondary" :icon="`pi ${isDark ? 'pi-sun' : 'pi-moon'}`" text rounded @click="toggleDark()" />
         </li>
         <li>
           <div>
-            <Avatar
-              image="https://apollo.primevue.org/demo/images/avatar/avatar.png"
-              shape="circle"
-              style="border: none; margin-left: 0"
-            />
+            <Avatar image="https://apollo.primevue.org/demo/images/avatar/avatar.png" shape="circle" style="border: none; margin-left: 0" />
           </div>
         </li>
       </ul>
--- packages/frontend/src/views/HomeView.vue
+++ packages/frontend/src/views/HomeView.vue
@@ -1,11 +1,3 @@
 <template>
-  <div class="flex flex-wrap gap-3 card justify-center">
-    <Button label="Primary" />
-    <Button label="Secondary" severity="secondary" />
-    <Button label="Success" severity="success" />
-    <Button label="Info" severity="info" />
-    <Button label="Warning" severity="warning" />
-    <Button label="Help" severity="help" />
-    <Button label="Danger" severity="danger" />
-  </div>
+  <h1>Tableau de bord</h1>
 </template>
