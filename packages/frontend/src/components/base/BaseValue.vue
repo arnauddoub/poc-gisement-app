a41a5f37780bb69de66b4f51c634c3b5d13f1c51
ArnaudDoubremelle <arnaud.doub@gmail.com>
2023-11-12 16:09:39 +0100

    feat: add Value component

---
+++ packages/frontend/src/components/base/BaseValue.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+interface Props {
+  vertical: boolean
+  label: string
+  value: string
+}
+
+withDefaults(defineProps<Props>(), {
+  vertical: true
+})
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <span class="text-sm text-gray-400">{{ label }}</span>
+    <span class="">{{ value }}</span>
+  </div>
+</template>
