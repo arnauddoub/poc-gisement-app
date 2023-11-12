b5dda3b7a46b013d1b4326193f1e5b13caaccc7c
ArnaudDoubremelle <arnaud.doub@gmail.com>
2023-11-12 16:09:30 +0100

    feat: add Heading component

---
+++ packages/frontend/src/components/base/BaseHeading.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{
+  type?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
+}>()
+</script>
+
+<template>
+  <h1 v-if="type === 'h1'" class="font-semibold text-2xl md:text-3xl"><slot /></h1>
+  <h2 v-else-if="type === 'h2'"><slot /></h2>
+  <h3 v-else-if="type === 'h3'"><slot /></h3>
+  <h4 v-else-if="type === 'h4'"><slot /></h4>
+  <h5 v-else-if="type === 'h5'"><slot /></h5>
+  <h6 v-else><slot /></h6>
+</template>
