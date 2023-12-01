789de5553dd025ca79fc7cea8b75a43ed934870b
ArnaudDoubremelle <arnaud.doub@gmail.com>
2023-12-02 00:11:18 +0100

    feat: add Property model

---
+++ packages/backend/database/factories/PropertyFactory.ts
@@ -0,0 +1,8 @@
+import Property from 'App/Models/Property'
+import Factory from '@ioc:Adonis/Lucid/Factory'
+
+export default Factory.define(Property, ({ faker }) => {
+  return {
+    //
+  }
+}).build()
---
+++ packages/backend/database/migrations/1701466490813_properties.ts
@@ -0,0 +1,28 @@
+import { PropertyStatus } from 'shared/enums/PropertyStatus'
+import { PropertyState } from 'shared/enums/PropertyState'
+import { PropertyCategory } from 'shared/enums/PropertyCategory'
+import { PropertyType } from 'shared/enums/PropertyType'
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'properties'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.enu('status', Object.values(PropertyStatus)).notNullable()
+      table.enu('state', Object.values(PropertyState)).notNullable()
+      table.enu('category', Object.values(PropertyCategory)).notNullable()
+      table.enu('type', Object.values(PropertyType)).notNullable()
+      table.float('surface').notNullable()
+      table.float('price').notNullable()
+      table.integer('location_id').unsigned().references('locations.id')
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
