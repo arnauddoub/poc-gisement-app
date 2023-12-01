ac058669bf6363a18478d7d41bc5e88b7ce5037b
ArnaudDoubremelle <arnaud.doub@gmail.com>
2023-12-02 00:11:29 +0100

    feat: add Location model

---
+++ packages/backend/database/factories/LocationFactory.ts
@@ -0,0 +1,8 @@
+import Location from 'App/Models/Location'
+import Factory from '@ioc:Adonis/Lucid/Factory'
+
+export default Factory.define(Location, ({ faker }) => {
+  return {
+    //
+  }
+}).build()
---
+++ packages/backend/database/migrations/1701466310270_locations.ts
@@ -0,0 +1,22 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'locations'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('address')
+      table.string('city')
+      table.string('zip_code')
+      table.float('latitude')
+      table.float('longitude')
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
