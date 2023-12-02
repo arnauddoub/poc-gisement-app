76cdfdc5977818c7656223a05d0ce41b38416401
ArnaudDoubremelle <arnaud.doub@gmail.com>
2023-12-02 19:46:53 +0100

    design: adjust column on properties view

--- packages/frontend/src/modules/property/composables/PropertyListItem.vue
+++ packages/frontend/src/modules/property/composables/PropertyListItem.vue
@@ -15,7 +15,7 @@ defineProps<{
       <div class="max-md:mx-auto max-md:mb-4 -bottom-1 left-0 w-20 md:absolute">
         <img src="@/assets/properties/building.png" class="opacity-70" alt="building" />
       </div>
-      <div class="flex max-md:flex-col flex-1 max-md:space-y-4 justify-between md:items-center">
+      <div class="flex-1 max-md:space-y-4 grid grid-cols-1 justify-between md:grid-cols-3 md:items-center">
         <div>
           <span class="text-sm mb-1 text-gray-400 block">{{ CATEGORY_LABELS[property.category] }}</span>
           <h3 class="font-medium max-w-sm text-lg leading-snug">
@@ -27,7 +27,7 @@ defineProps<{
           <BaseValue label="Statut :" :value="STATE_LABELS[property.state]" />
           <BaseValue label="Type :" :value="TYPE_LABELS[property.type]" />
         </div>
-        <div class="md:w-40">
+        <div class="md:ml-auto md:w-40">
           <RouterLink :to="{ name: 'properties.show', params: { id: property.id } }">
             <Button class="w-full justify-center">Détail</Button>
           </RouterLink>
