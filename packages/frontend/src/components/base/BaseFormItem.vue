23cd11528aeb73d542050b86e085a6c8aabbafcd
ArnaudDoubremelle <arnaud.doub@gmail.com>
2023-12-03 22:14:49 +0100

    feat: add form item component

---
+++ packages/frontend/src/components/base/BaseFormItem.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+  label?: string
+  forId?: string
+}>()
+</script>
+
+<template>
+  <div>
+    <label :for="forId" class="font-medium mb-2 block">{{ label }}</label>
+    <slot />
+  </div>
+</template>
